e43acb1d9a4cf0574eb657c5ec487a05ffc613b8
subutux <subutux@gmail.com>
2012-01-17 11:54:20 +0100

    fixed typo in require once

--- example.php
+++ example.php
@@ -1,5 +1,5 @@
 <?
-require_once("mailAth.class.php");
+require_once("mailAuth.class.php");
 $a = new mailAuth("mail.server.com",array("imap","ssl","novalidate-cert"),"INBOX");
 if(!$a->auth("test@mail.com","test1")) {
         echo "\nThere was an error authentication you:\n";
